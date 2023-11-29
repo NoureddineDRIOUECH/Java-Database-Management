c4d205e33ecc945845ed0ea369c677f2267ceaf8
NoureddineDRIOUECH <nourddinedriouech@gmail.com>
2023-11-29 15:36:21 +0100

    The main app

--- src/main/java/com/nomz/javadatabasemanagement/app.java
+++ src/main/java/com/nomz/javadatabasemanagement/app.java
@@ -10,18 +10,6 @@ import java.util.Scanner;
 
 public class app {
     public static void main(String[] args) {
-    String dbName;
-    System.out.print("Enter DB name: ");
-    Scanner in = new Scanner(System.in);
-    dbName = in.nextLine();
-//    DBcreation.createDatabase(dbName);
-//    Connection connection = DBconnection.getConnection(dbName);
-//    if (connection == null) {
-//        System.out.println("Failed to establish a connection.");
-//    } else {
-//        System.out.println("Connection established successfully.");
-////    }
-   DBdeletion.deleteDatabase(dbName);
-    System.out.println("done");
+        
 }
 }
