2262b41e27c9b004bbc4f4fe04161c701b173546
saijmohammed10 <129989968+saijmohammed@users.noreply.github.com>
2023-12-02 23:08:11 +0100

    Add files via upload
    supprimer les tables

---
+++ src/main/java/com/nomz/javadatabasemanagement/dataaccessobject/DBTableDeletion.java
@@ -0,0 +1,25 @@
+<<<<<<< HEAD
+package com.nomz.javadatabasemanagement.dataaccessobject;public class DBTableDeletion {
+=======
+package com.nomz.javadatabasemanagement.dataaccessobject;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class DBTableDeletion {
+    public static void dropTable(String dbName, String tableName) {
+        Connection con = DBconnection.getConnection(dbName);
+        try {
+            String dropTableSQL = "DROP TABLE IF EXISTS " + tableName;
+
+            try (PreparedStatement dropTableStatement = con.prepareStatement(dropTableSQL)) {
+                dropTableStatement.executeUpdate();
+                System.out.println("Table '" + tableName + "' supprimée avec succès.");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+>>>>>>> cfd6a6626ff97798ef6f9a6a18e1a3718d6b37d0
+}
